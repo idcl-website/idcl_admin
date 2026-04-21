6e3554d18e0aeec3d954813cc619045a70733da6
jephthahdominic <stdominicjephthah63@gmail.com>
2026-04-21 20:37:34 +0100

    increased the timout to 20sec

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,7 +12,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
-  timeout: 10000,
+  timeout: 20000,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
